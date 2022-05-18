e6d0456a50ff8e1a60d880bb035b3943b9b1e96a
Netydevelopment <105376707+netydevelopment@users.noreply.github.com>
2022-05-18 16:49:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ async def on_ready():
 
 keywords = [
     "17ebook.co", "aladel.net", "bpwhamburgorchardpark.org", "clicnews.com",
-    "iplogger", "2no.co", "yip.su", "ipgrabber", "iplis.ru", "02ip.ru", "partpicker.shop","sportshub.bar", "locations.quest", "lovebird.guru", "trulove.guru", "dateing.club", "shrekis.life", "headshot.monster", "gaming-at-my.best", "progaming.monster", "yourmy.monster", "imageshare.best", "screenshot.best", "gamingfun.me", "catsnthing.com", "catsnthings.fun", "yt118.com", "stock888.cn", "curiouscat.club", "joinmy.site", "fortnitechat.site", "fortnight.space", "freegiftcards.co", "leancoding.co", "grabify.link", "youareanidiot.org", "fantasticfilms.ru", "gardensrestaurantandcatering.com", "clicnews.com", "ginedis.com", "dfwdiesel.net", "seksburada.net", "tathli.com", "teamclouds.com", "xnescat.info", "magic4you.nu", "Homicide.igarape.org.br", "Goodbyewarden.com", "kekma.net", "Worldsbirthsanddeaths.com", "ctbto.org", "www.ps3cfw.com", "", "", "", "", "", "", "", "", "", "", ""
+    "iplogger", "2no.co", "yip.su", "ipgrabber", "iplis.ru", "02ip.ru", "partpicker.shop","sportshub.bar", "locations.quest", "lovebird.guru", "trulove.guru", "dateing.club", "shrekis.life", "headshot.monster", "gaming-at-my.best", "progaming.monster", "yourmy.monster", "imageshare.best", "screenshot.best", "gamingfun.me", "catsnthing.com", "catsnthings.fun", "yt118.com", "stock888.cn", "curiouscat.club", "joinmy.site", "fortnitechat.site", "fortnight.space", "freegiftcards.co", "leancoding.co", "grabify.link", "youareanidiot.org", "fantasticfilms.ru", "gardensrestaurantandcatering.com", "clicnews.com", "ginedis.com", "dfwdiesel.net", "seksburada.net", "tathli.com", "teamclouds.com", "xnescat.info", "magic4you.nu", "Homicide.igarape.org.br", "Goodbyewarden.com", "kekma.net", "Worldsbirthsanddeaths.com", "ctbto.org", "www.ps3cfw.com"
 ] 
 
 https = ["https://", "http://"]
@@ -30,4 +30,4 @@ async def on_message(message):
                 embed.set_footer(text=f"{message.author} sent dangerous message/link in {message.channel}")
                 await message.channel.send(embed=embed)
 
-client.run("OTc0NDAxMzc2NzMxOTQ3MDA4.G0BE7P.d7Zx8b62syt6EcFQuMB2VRqUKphykCxzt9lbEA")
+client.run("enter your token in here")
